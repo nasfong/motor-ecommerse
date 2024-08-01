39bab73b9ebfa8f6c0d23ccd067afebd51619e22
Samnang Tangfong <samnang.tangfong@DESTANGFONG.sabay.office>
2024-08-01 14:04:47 +0700

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import { Button } from "@/components/ui/button";
-// import Tabs from "@/components/Tabs";
+import Tabs from "@/components/Tabs";
 // import Slider from "@/components/Slider";
 import Link from "next/link";
 // import ProductList from "@/components/ProductList";
 
-// async function getData(): Promise<Type[]> {
-//   const res = await fetch('http://localhost:5000/api/type', { cache: 'no-store' })
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
-//   return res.json()
-// }
+async function getData(): Promise<Type[]> {
+  const res = await fetch('http://localhost:5000/api/type', { cache: 'no-store' })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
 export default async function HomePage() {
-  // const data = await getData()
+  const data = await getData()
 
   return (
     <main className="flex flex-col gap-5">
       {/* <Slider /> */}
-      {/* <Tabs data={data} /> */}
+      <Tabs data={data} />
       {/* <ProductList /> */}
       <div className='text-center'>
         <Link href='/all-product'>
