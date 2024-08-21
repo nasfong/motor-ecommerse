94851652bf0d69b6da65ce933e7c12deaec8abb8
Samnang Tangfong <samnang.tangfong@DESTANGFONG.sabay.office>
2024-08-21 16:56:07 +0700

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,25 +5,23 @@ import type { MetadataRoute } from 'next'
 const domain = 'https://www.kyhanmotorshop.store';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const locale = 'kh'
   const queryClient = new QueryClient();
 
-  // Prefetch products to cache them before rendering the sitemap
   await queryClient.prefetchQuery({
     queryKey: ['products'],
     queryFn: () => getProduct(),
   });
 
-  // Assuming getProduct() returns a list of products, but your original code referenced 'blog'
   const products = await queryClient.getQueryData(['products']) as Product[];
 
-  // Ensure you have blog data or replace this with product data or similar
   const posts = products.map(({ _id, name }) => ({
-    url: `${domain}/all-product/${_id}/${name}`, // Adjust the URL structure based on your app
-    lastModified: new Date().toISOString(), // Fallback to current date if _updatedAt is not provided
+    url: `${domain}/${locale}/all-product/${_id}/${encodeURIComponent(name)}`,
+    lastModified: new Date().toISOString(),
   }));
 
   const routes = ["", "/all-product", "/contact"].map((route) => ({
-    url: `${domain}${route}`,
+    url: `${domain}/${locale}${route}`,
     lastModified: new Date().toISOString(),
   }));
 
