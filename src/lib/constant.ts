d06b3f9ed079854afa4d3b35a06f5271c22aa068
Nas Fong <fongren007@gmail.com>
2024-09-08 23:20:44 +0700

    toktok url

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -5,57 +5,57 @@ import { Phone } from "lucide-react";
 // Create a function to get the translated data based on the locale
 export const stocks = (t: (s: string) => void) => {
   return [
-    { _id: 1, name: t('In Stock') },
-    { _id: 2, name: t('Out Stock') }
+    { _id: 1, name: t("In Stock") },
+    { _id: 2, name: t("Out Stock") },
   ];
 };
 
 export const location = {
   lat: 12.717218,
-  lng: 104.887384
+  lng: 104.887384,
 };
 
-export const imageUrl = `${process.env.NEXT_PUBLIC_API_URL}/image/`
-export const default_image = '/images/default-image.jpg'
+export const imageUrl = `${process.env.NEXT_PUBLIC_API_URL}/image/`;
+export const default_image = "/images/default-image.jpg";
 
 const phone = [
   {
     icon: Phone,
-    name: 'Phone Number',
-    value: '096 255 5577',
-    color: '#90bb21',
-    url: 'tel:+855962555577'
+    name: "Phone Number",
+    value: "096 255 5577",
+    color: "#90bb21",
+    url: "tel:+855962555577",
   },
   {
     icon: Phone,
-    name: 'Phone Number',
-    value: '066 300 016',
-    color: '#90bb21',
-    url: 'tel:+85566300016'
-  }
-]
+    name: "Phone Number",
+    value: "066 300 016",
+    color: "#90bb21",
+    url: "tel:+85566300016",
+  },
+];
 
 const facebook = {
   icon: Facebook02Icon,
-  name: 'Facebook',
-  value: 'ហាងម៉ូតូ គី ហាន',
-  color: '#227bf3',
-  url: 'https://www.facebook.com/KyhanMotorShop'
-}
+  name: "Facebook",
+  value: "ហាងម៉ូតូ គី ហាន",
+  color: "#227bf3",
+  url: "https://www.facebook.com/KyhanMotorShop",
+};
 const telegram = {
   icon: TelegramIcon,
-  name: 'Telegram',
-  value: 'Sopheaktra Ky',
-  color: '#31aae9',
-  url: 'https://t.me/sopheaktraky'
-}
+  name: "Telegram",
+  value: "Sopheaktra Ky",
+  color: "#31aae9",
+  url: "https://t.me/sopheaktraky",
+};
 const tiktok = {
   icon: TiktokIcon,
-  name: 'TikTok',
-  value: 'Sopheaktra Ky',
-  color: '#080808',
-  url: 'https://t.me/sopheaktraky'
-}
+  name: "TikTok",
+  value: "Sopheaktra Ky",
+  color: "#080808",
+  url: "https://www.tiktok.com/@kyhan8821999?_t=8pPs3J2f3SL&_r=1",
+};
 
 export const Constant = {
   stocks,
@@ -65,4 +65,4 @@ export const Constant = {
   facebook,
   telegram,
   tiktok,
-}
+};
